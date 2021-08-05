e375333b25c7d8eaeb0327f49bbe65f66ae53fe9
Milton Bueno <miltonjunior17@gmail.com>
2021-08-05 17:41:40 -0300

    Quarto commit - Criando live template do MobX

---
+++ lib/Home_Controller.dart
@@ -0,0 +1,10 @@
+//Live template - mobx
+
+// import 'package:mobx/mobx.dart';
+// part 'Home_Controller.g.dart';
+//
+// class HomeController = HomeControllerBase with _$HomeController;
+//
+// abstract class HomeControllerBase with Store{
+//
+// }
